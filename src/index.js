a395cd912ced77ab6a6e1c633b3ba211dc4dab6f
KaranSonik <sonikkaran@gmail.com>
2022-10-28 14:08:33 +0100

    > setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Landing from './pages/Landing';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {BrowserRouter, Switch } from ''
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Landing/>
   </React.StrictMode>
 );
 
---
+++ src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './landing.css'
+
+const Landing = () => {
+  return (
+    <div className='landing'>
+        <div>
+            <h2>Landing Page</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
